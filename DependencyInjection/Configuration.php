954612d7588a29a0fdf18d3bd985c6781ff00865
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-09-11 17:41:10 +0400

    add services

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,6 +20,14 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('blockchainrndhub_ethereum');
 
+        $rootNode->
+            children()
+                ->scalarNode('etherscan_api_key')
+                ->isRequired()
+                ->cannotBeEmpty()
+                ->end()
+        ->end()
+        ;
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
