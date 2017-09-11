13a6a78a50a48da1bfcedd7a260ca07107b989e7
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-09-11 16:01:09 +0400

    changes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Blockchainrndhub\Ethereum\DependencyInjection;
+namespace Blockchainrndhub\EthereumBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
