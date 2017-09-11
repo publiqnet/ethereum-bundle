a2d43b0049a82a8ebfefd3175a25d393c14e3df2
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-09-11 15:39:21 +0400

    test

--- DependencyInjection/BlockchainrndhubEthereumExtension.php
+++ DependencyInjection/BlockchainrndhubEthereumExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace blockchainrndhub\ethereumBundle\DependencyInjection;
+namespace Blockchainrndhub\EthereumBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -8,11 +8,11 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration.
+ * This is the class that loads and manages your bundle configuration
  *
- * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class blockchainrndhubethereumExtension extends Extension
+class BlockchainrndhubEthereumExtension extends Extension
 {
     /**
      * {@inheritdoc}
@@ -22,7 +22,7 @@ class blockchainrndhubethereumExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace blockchainrndhub\ethereumBundle\DependencyInjection;
+namespace Blockchainrndhub\Ethereum\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files.
+ * This is the class that validates and merges configuration from your app/config files
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
  */
 class Configuration implements ConfigurationInterface
 {
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('blockchainrndhubethereum');
+        $rootNode = $treeBuilder->root('blockchainrndhub_ethereum');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
